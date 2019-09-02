38429a0a627d693dcd041b9124ffa6d297d70ca0
Guven Yildiz <guven.yildiz@gmail.com>
2019-09-02 02:47:27 +0000

    Initial commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,17 @@
 class QuotesController < ApplicationController
-            def index
-            end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
+
+  def new
+    @quote = Quote.new
+  end
+  def create
+            Quote.create(quote_params)
+            redirect_to root_path
+  end
+  private
+  def quote_params
+            params.require(:quote).permit(:saying, :author)
+end
 end
